a8d819c028e2a3f0833bab54be469a6629efd723
viricruz <viridiana.cruz.olivera@gmail.com>
2020-04-22 16:55:05 -0700

    Add initial files to run react app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Initial setup</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
