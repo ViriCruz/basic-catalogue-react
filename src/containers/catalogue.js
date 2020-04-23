6ce5cdd7a3b8af0c086df84dd798a7c038bbd2db
viricruz <viridiana.cruz.olivera@gmail.com>
2020-04-22 17:10:59 -0700

    Add empty unordered list to render function

---
+++ src/containers/catalogue.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Catalogue extends React.Component {
+  render() {
+    return(
+      <div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Catalogue
