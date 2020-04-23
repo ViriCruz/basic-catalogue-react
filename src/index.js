eed8b009f54fa228c5e604ea8ee26de2e0e88a25
viricruz <viridiana.cruz.olivera@gmail.com>
2020-04-22 17:11:14 -0700

    Add App component to render function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from './components/app'
 
 ReactDOM.render(
   <React.StrictMode>
     <h1>Initial setup</h1>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
