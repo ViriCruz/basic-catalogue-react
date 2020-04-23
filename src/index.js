55dbe9bc91bed51fcc8e70141724b4085bc7a733
viricruz <viridiana.cruz.olivera@gmail.com>
2020-04-23 11:26:25 -0700

    Add Provider component to connect redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers/root-reducer'
 import App from './components/app'
 
+const initialState = {
+  pending: false,
+  pokemons: [],
+  error: null
+}
+
+const store = createStore(rootReducer, {
+  data: initialState
+})
+
+console.log(store.getState())
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Initial setup</h1>
-    <App/>
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
