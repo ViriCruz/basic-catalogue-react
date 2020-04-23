413d13cd9a7c6bc817d82ce670dcee4a973bbdee
viricruz <viridiana.cruz.olivera@gmail.com>
2020-04-23 15:53:50 -0700

    Add componentDidMount to fetch api data and render list of pokemons

--- src/containers/catalogue.js
+++ src/containers/catalogue.js
@@ -1,17 +1,49 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import fetchPokemonsAction from '../api/fetch-pokemons'
+import { bindActionCreators } from 'redux'
+import { getPokemonsError, getPokemons, getPokemonsPending } from '../reducers/pokemon-reducer'
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  fetchPokemons: fetchPokemonsAction
+}, dispatch);
+
+const mapStateToProps = state => ({
+  data: {
+    error: getPokemonsError(state),
+    pokemons: getPokemons(state),
+    pending: getPokemonsPending(state)
+  }
+});
 
 class Catalogue extends React.Component {
+  constructor(props){
+    super(props)    
+  }
+
+  async componentDidMount() {
+    const {fetchPokemons} = this.props
+    fetchPokemons('normal')
+  }
+
   render() {
+    const {error, pending, pokemons} = this.props.data
+    if(error) {
+      return <div>Error! {error} </div>
+    }
+
+    if(pending) {
+      return <div>loading..</div>
+    }
+    
     return(
       <div>
         <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
+          {pokemons.map(pokemon =><li>{pokemon.pokemon.name}</li>)}
         </ul>
       </div>
     )
   }
 }
 
-export default Catalogue
+export default connect(mapStateToProps, mapDispatchToProps)(Catalogue)
