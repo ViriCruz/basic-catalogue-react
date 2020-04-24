169f2bbff899f07ab3fe2800f1b942dace28d97d
viricruz <viridiana.cruz.olivera@gmail.com>
2020-04-24 15:12:08 -0700

    Add cases for fetching a single pokemon

--- src/reducers/pokemon-reducer.js
+++ src/reducers/pokemon-reducer.js
@@ -1,26 +1,50 @@
-import {FETCH_POKEMONS_PENDING, FETCH_POKEMONS_SUCCESS, FETCH_POKEMONS_ERROR} from '../actions/index';
+import {
+    FETCH_POKEMONS_PENDING, 
+    FETCH_POKEMONS_SUCCESS, 
+    FETCH_POKEMONS_ERROR,
+    FETCH_SINGLE_POKEMON_ERROR,
+    FETCH_SINGLE_POKEMON_PENDING,
+    FETCH_SINGLE_POKEMON_SUCCESS,
+} from '../actions/index';
 
 export const pokemonsReducer = (state = {}, action) => {
     switch(action.type) {
-        case FETCH_POKEMONS_PENDING: 
-            return {
-                ...state,
-                pending: true
-            }
-        case FETCH_POKEMONS_SUCCESS:
-            return {
-                ...state,
-                pending: false,
-                pokemons: action.pokemons
-            }
-        case FETCH_POKEMONS_ERROR:
-            return {
-                ...state,
-                pending: false,
-                error: action.error
-            }
-        default: 
-            return state;
+			case FETCH_POKEMONS_PENDING: 
+				return {
+					...state,
+					pending: true
+				}
+			case FETCH_POKEMONS_SUCCESS:
+				return {
+					...state,
+					pending: false,
+					pokemons: action.pokemons
+				}
+			case FETCH_POKEMONS_ERROR:
+				return {
+					...state,
+					pending: false,
+					error: action.error
+				}
+			case FETCH_SINGLE_POKEMON_PENDING:
+				return {
+					...state,
+					pending: true
+				}
+			case FETCH_SINGLE_POKEMON_SUCCESS:
+				return {
+					...state,
+					pending:false,
+					pokemons: [action.pokemons]
+				}
+			case FETCH_SINGLE_POKEMON_ERROR:
+				return{
+					...state,
+					pending:false,
+					error: action.error
+				}		    
+			default: 
+				return state;
     }
 }
 
