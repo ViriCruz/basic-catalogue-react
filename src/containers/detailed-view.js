57fc639d30f62c84761eccddd373accc8cd05163
viricruz <viridiana.cruz.olivera@gmail.com>
2020-04-24 15:10:22 -0700

    Add react-router as a dependency

---
+++ src/containers/detailed-view.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pokemon from '../components/item-detailed-view'
+
+class DetailedView extends React.Component {
+  render() {
+    return(
+      <Pokemon />
+    )
+  }
+}
+
+export default DetailedView
