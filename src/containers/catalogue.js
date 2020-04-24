9b3e4643e2d7157ad30599a86639ff4696f90a0b
viricruz <viridiana.cruz.olivera@gmail.com>
2020-04-24 11:39:54 -0700

    Add Link to render pokemon detailed view

--- src/containers/catalogue.js
+++ src/containers/catalogue.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux'
 import fetchPokemonsAction from '../api/fetch-pokemons'
 import { bindActionCreators } from 'redux'
 import { getPokemonsError, getPokemons, getPokemonsPending } from '../reducers/pokemon-reducer'
-
+import whoIs from '../assets/who-is.png'
+import { Link } from 'react-router-dom'
 const mapDispatchToProps = dispatch => bindActionCreators({
   fetchPokemons: fetchPokemonsAction
 }, dispatch);
@@ -26,6 +27,10 @@ class Catalogue extends React.Component {
     fetchPokemons('normal')
   }
 
+  handleView(){
+    console.log('handle view')
+  }
+
   render() {
     const {error, pending, pokemons} = this.props.data
     if(error) {
@@ -38,8 +43,16 @@ class Catalogue extends React.Component {
     
     return(
       <div>
-        <ul>
-          {pokemons.map(pokemon =><li>{pokemon.pokemon.name}</li>)}
+        <ul className="pokelist">
+          {pokemons.map(pokemon => <li 
+            className="pokemon" 
+            onClick={this.handleView}>
+            <Link to={`/pokemon/${pokemon.pokemon.name}`}>
+              <img className="pokemon-pic" src={whoIs}/>
+              {pokemon.pokemon.name}
+            </Link>
+            </li>)
+          }
         </ul>
       </div>
     )
