c3ab46748599fe43dd1a1d3cb051fca6b47d82b2
viricruz <viridiana.cruz.olivera@gmail.com>
2020-04-24 11:39:05 -0700

    Add Router and Switch to render a specific component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Catalogue from '../containers/catalogue'
+import Pokemon from '../components/item-detailed-view'
 
 const app = () => {
   return(
-    <Catalogue />
+    <Router>
+      <div>
+        <h1>Pokemons!</h1>
+        <div>
+          <Switch>
+            <Route exact path="/"> <Catalogue /> </Route>
+            <Route path="/pokemon/:name"> <Pokemon /> </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   )
 }
 
