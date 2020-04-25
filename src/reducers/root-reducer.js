49a74b2348ee89c9fb4b30acaca68cde47168b40
viricruz <viridiana.cruz.olivera@gmail.com>
2020-04-24 17:41:49 -0700

    Add filter reducer to root reducer

--- src/reducers/root-reducer.js
+++ src/reducers/root-reducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux'
 import { pokemonsReducer } from './pokemon-reducer'
-
+import filterType from './filter-reducer'
 const rootReducer = combineReducers({
   data: pokemonsReducer,
+  filter: filterType,
 })
 
 export default rootReducer
