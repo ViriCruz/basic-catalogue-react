75027582b88945d5308443318c449023f055129f
viricruz <viridiana.cruz.olivera@gmail.com>
2020-05-05 15:09:25 -0700

    Add unit tests for itemDetailedView component

---
+++ src/__tests__/components/itemDetailedView.test.js
@@ -0,0 +1,55 @@
+import { mount } from 'enzyme';
+import Pokemon from '../../components/itemDetailedView'
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+describe('itemDetailedView test', () => {
+
+  let wrapper;
+  let pokemon = {
+    name: 'charizard',
+    abilities: [
+      {
+        "ability": {
+          "name": "solar-power",
+          "url": "https://pokeapi.co/api/v2/ability/94/"
+        },
+        "is_hidden": true,
+        "slot": 3
+      }
+    ],
+    sprites: {
+      "front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
+    },
+    stats: [
+      {
+        "base_stat": 100,
+        "effort": 0,
+        "stat": {
+            "name": "speed",
+            "url": "https://pokeapi.co/api/v2/stat/6/"
+        }
+      }
+    ],
+  }
+  
+  beforeEach(() => {
+    wrapper = mount(
+      <Router>
+        <Pokemon pokemon={pokemon} />
+      </Router>
+    )
+  })
+
+  it('renders pokemon image', () => {
+    expect(wrapper.find({alt: pokemon.name})).toHaveLength(1)
+  })
+
+  it('renders pokemon abilities',() => {
+    expect(wrapper.find('.abilities')).toHaveLength(1)
+  })
+
+  it('renders pokemon stats',() => {
+    expect(wrapper.find('.stats')).toHaveLength(1)
+  })
+})
